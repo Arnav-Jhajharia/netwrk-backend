73749c763765345d2f39c962257a40190f69a759
unknown <adityavikramsinha19@gmail.com>
2025-03-25 15:49:54 +0530

    added avro reading from incoming stream, and prints it to console. Ideally what we will want is to create reader schema that only checks for a certain number of fields wihout deserialising everything (put it to top?), but I am honestly tired of doing everything myself. So, unless someone else decides to make a schema (w & r), I dont think I will be doing anything more for avro reading downstream & upstream.

--- src/streams.rs
+++ src/streams.rs
@@ -1,15 +1,23 @@
 use actix_web::{web, HttpRequest, HttpResponse};
 use actix_web::web::BytesMut;
+use apache_avro::Reader;
 use futures_util::StreamExt as _;
+
+
+
 
 /// Handles Streams
 #[actix_web::post("/streams")]
 async fn streams (mut payload : web::Payload, _req : HttpRequest) -> HttpResponse {
-
-    let mut body = BytesMut::new();
+    let mut body = BytesMut::new() ;
     while let Some(chunk) = payload.next().await {
         let chunk = chunk.unwrap();
         body.extend_from_slice(&chunk);
     }
-    HttpResponse::Ok().body(body)
+    let bytes_vec = body.to_vec();
+    let r = Reader::new(&(bytes_vec)[..]).unwrap();
+    for value  in r {
+        println!("{:#?}",value.unwrap());
+    }
+    HttpResponse::Ok().body("Ok")
 }
